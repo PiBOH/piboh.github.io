47fa4cc8b788ac8e1c49568fbd1a5a4be8c0e60b
PiBOH <piboh.github@gmail.com>
2026-06-30 22:10:44 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig(async ({ mode }) => {
-  const plugins = [react(), tailwindcss()];
-  try {
-    // @ts-ignore
-    const m = await import('./.vite-source-tags.js');
-    plugins.push(m.sourceTags());
-  } catch {}
-
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), ['VITE_', 'NEXT_PUBLIC_']);
   const processEnvDefines: Record<string, string> = {};
   for (const [key, value] of Object.entries(env)) {
@@ -18,7 +10,7 @@ export default defineConfig(async ({ mode }) => {
   }
 
   return {
-    plugins,
+    plugins: [react(), tailwindcss()],
     base: './',
     envPrefix: ['VITE_', 'NEXT_PUBLIC_'],
     define: processEnvDefines,
